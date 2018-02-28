7b8f4e3fdec5ecc86fd67230e20de60da3e0a0cd
Rob Nelson <nymbyl@gmail.com>
2018-02-27 23:39:34 -0500

    changed fuel http requests to be async - but it made no difference as far as timing (which makes sense)

--- src/main/kotlin/duke/Widgets.kt
+++ src/main/kotlin/duke/Widgets.kt
@@ -1,5 +1,6 @@
 package duke
 
+import com.github.kittinunf.fuel.core.FuelError
 import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -21,6 +22,8 @@ import com.natpryce.konfig.getValue
 import com.natpryce.konfig.PropertyGroup
 
 import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import com.github.kittinunf.fuel.core.ResponseDeserializable
@@ -66,58 +69,60 @@ data class WidgetsPerson(val uri: String, val label: String, val attributes: Map
     }
 }
 
-fun widgetsPersonResult(person: Person): Deferred<String> {
+fun widgetsPersonResult(person: Person): Deferred<Triple<Request, Response, Result<WidgetsPerson, FuelError>>> {
     var uri = person.uri
     var path = "/widgets/api/v0.9/people/complete/all.json?uri=$uri"
 
     return async(CommonPool) {
-        // NOTE: this is sync version
-        val (request, response, result) = path.httpGet().responseObject(WidgetsPerson.Deserializer())
-
-        //path.httpGet().responseObject(WidgetsPerson.Deserializer()) { request, response, result ->
-        val (person, error) = result
-
-        var flag: Boolean = false
-        var summary: String = ""
-
-        if (person != null) {
-            val imageUri = person.attributes["imageUri"]
-            flag = true
-            summary = "widgets:imageUri=$imageUri $flag"
-        } else {
-            flag = false
-            summary = "widget:null $flag"
-        }
-        //return
-        summary
+        path.httpGet().responseObject(WidgetsPerson.Deserializer())
     }
 }
 
-fun vivoPersonResult(person: Person): Deferred<String> {
+fun vivoPersonResult(person: Person): Deferred<Triple<Request, Response, Result<String, FuelError>>> {
     var uri = person.uri
 
     var path = uri.replace("https://scholars.duke.edu", "").replace("/individual/", "/display/")
 
     return async(CommonPool) {
-        val (request, response, result) = path.httpGet().responseString()
-        val (data, error) = result
+        path.httpGet().responseString()
+    }
+}
 
-        var flag: Boolean = false
-        var summary: String = ""
+fun parseVivoResponse(response: Response, result: Result<String, FuelError>): String {
+    val (data, error) = result
 
-        when (result) {
-            is Result.Failure -> { flag = false }
-            is Result.Success -> {
-                val html = data as String
+    var flag: Boolean = false
+    var summary: String = ""
 
-                when {
-                    html.contains("img class=\"individual-photo\"") -> { flag = true }
-                }
+    when (result) {
+        is Result.Failure -> { flag = false }
+        is Result.Success -> {
+            val html = data as String
+
+            when {
+                html.contains("img class=\"individual-photo\"") -> { flag = true }
             }
         }
-        summary = "vivo $path: $flag"
-        summary
     }
+    summary = "vivo ${response.url}: $flag"
+    return summary
+}
+
+fun parseWidgetsResponse(result: Result<WidgetsPerson, FuelError>): String {
+    val (person, error) = result
+
+    var flag: Boolean = false
+    var summary: String = ""
+
+    if (person != null) {
+        val imageUri = person.attributes["imageUri"]
+        flag = true
+        summary = "widgets:imageUri=$imageUri $flag"
+    } else {
+        flag = false
+        summary = "widget:null $flag"
+    }
+    return summary
 }
 
 fun asyncProcessor(channel: ReceiveChannel<Person>, results: MutableMap<Person, ArrayList<String>>,
@@ -126,10 +131,13 @@ fun asyncProcessor(channel: ReceiveChannel<Person>, results: MutableMap<Person, 
         println("uri:${person.uri}, thread:$thread")
 
         val widgets = widgetsPersonResult(person)
-        val widgetsSummary = widgets.await()
+
+        val (_, _, wResult) = widgets.await()
+        val widgetsSummary = parseWidgetsResponse(wResult)
 
         val vivo = vivoPersonResult(person)
-        val vivoSummary = vivo.await()
+        val (_, vResponse, vResult) = vivo.await()
+        val vivoSummary = parseVivoResponse(vResponse, vResult)
 
         results[person] = ArrayList<String>()
         results[person]?.add(widgetsSummary)
@@ -195,7 +203,4 @@ fun main(args: Array<String>) = runBlocking<Unit> {
     }
     println("*** results: $size****")
     println("*** took: $timeElapsed ms***")
-
-    // *** took: 28773 ms***
-    // *** took: 120292 ms***
 }
